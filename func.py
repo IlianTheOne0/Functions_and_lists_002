842be847b55d14fbfb637ea55d390b15754e3a90
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-27 23:07:47 +0200

    Completed Task 2

--- func.py
+++ func.py
@@ -1,5 +1,17 @@
 def main():
     try:
-        pass
+        a = int(input('Enter the first number: '))
+        b = int(input('Enter the second number: '))
+
+        print(is_even(a, b))
     except Exception as e:
         print(f'Error: {e}')
+
+def is_even(a, b):
+    result = str()
+
+    for i in range(a, b + 1):
+        if i % 2 == 0:
+            result += str(i) + ' '
+
+    return result
