78e503bbf8376bc32ed0270361c11f00f12f5ef6
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-28 00:27:40 +0200

    Started

--- func.py
+++ func.py
@@ -1,5 +1,16 @@
 def main():
-    try:
-        pass
-    except Exception as e:
-        print(f'Error: {e}')
+    print('The lowest number is:', search_for_the_least(split(input('Enter numbers, separated by a space: '), ' ')))
+
+def split(numbers, pattern):
+    for number in pattern:
+        numbers = numbers.replace(number, ' ')
+    return numbers.split()
+
+def search_for_the_least(numbers):
+    lowest = numbers[0]
+
+    for i in range(len(numbers) - 1):
+        if numbers[i + 1] < lowest:
+            lowest = numbers[i + 1]
+
+    return lowest
