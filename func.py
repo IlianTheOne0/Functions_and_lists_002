a177908f2200f605de95fe4aebbbcad7198da318
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-27 23:58:25 +0200

    Completed Task 3

--- func.py
+++ func.py
@@ -1,13 +1,32 @@
 def main():
-    try:
-        size = int(input('Size: '))
-        symbol = str(input('Symbol: '))
-        state = bool(input('Filled or empty (True, False): '))
-    except Exception as e:
-        print(f'Error: {e}')
+    while True:
+        try:
+            size = int(input('Size: '))
+            symbol = input('Symbol: ')
+            state = input('Filled or empty (True, False): ')
+
+            if state not in ['True', 'False']:
+                raise ValueError(f"invalid literal for bool(): '{state}'")
+            break
+        except Exception as e:
+            print(f'Error: {e}')
+            continue
+
+    painter(size, symbol, state)
 
 def painter(size, symbol, state):
-    for i in range(0, 11):
-        for j in range(0, 11):
+    if state == 'True':
+        for y in range(0, size):
+            for x in range(0, size):
+                print(symbol, end='  ')
+            print()
+    else:
+        print((symbol + '  ') * size)
+
+        for y in range(0, size - 2):
             print('*', end='  ')
-        print()
+            print('.  ' * (size - 2), end='')
+            print('*', end='  ')
+            print()
+
+        print((symbol + '  ') * size)
