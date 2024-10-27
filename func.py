7653b601eda614637d2c066296ba96f9b3f78ea7
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-27 23:11:57 +0200

    Started

--- func.py
+++ func.py
@@ -1,5 +1,13 @@
 def main():
     try:
-        pass
+        size = int(input('Size: '))
+        symbol = str(input('Symbol: '))
+        state = bool(input('Filled or empty (True, False): '))
     except Exception as e:
         print(f'Error: {e}')
+
+def painter(size, symbol, state):
+    for i in range(0, 11):
+        for j in range(0, 11):
+            print('*', end='  ')
+        print()
