00ebc2b709777d1e4a192d962986d1fba6e2d2a0
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-28 15:56:26 +0200

    Completed Task 7

--- func.py
+++ func.py
@@ -1,5 +1,30 @@
 def main():
-    try:
-        pass
-    except Exception as e:
-        print(f'Error: {e}')
+    while True:
+        try:
+            number = int(input('Enter the number: '))
+            break
+        except Exception as e:
+            print(f'Error: {e}')
+            continue
+
+    number = is_negative(number)
+
+    print('Is this number a palindrome?:', is_palindrome(number))
+
+def is_negative(number):
+    return (number - (number * 2)) if number < 0 else number
+
+def is_palindrome(number):
+    result = ''
+    original_value = number
+
+    if len(str(original_value)) != 1:
+        while number:
+            digit = number % 10
+            number //= 10
+
+            result += str(digit)
+
+        return int(result) == original_value
+    else:
+        return True
