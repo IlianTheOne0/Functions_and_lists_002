daa637c166d41c6133bc0a0631dfa3c8fd73f865
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-28 14:48:17 +0200

    Completed Task 5

--- func.py
+++ func.py
@@ -1,5 +1,24 @@
 def main():
-    try:
-        pass
-    except Exception as e:
-        print(f'Error: {e}')
+    while True:
+        try:
+            a = int(input('Enter the first number: '))
+            b = int(input('Enter the second number: '))
+            break
+        except Exception as e:
+            print(f'Error: {e}')
+            continue
+
+    a, b = is_the_biggest_check(a, b)
+    print(f'Product of numbers in the range {a} and {b}: {multiplication(a, b)}')
+
+def is_the_biggest_check(a, b):
+    a, b = (b, a) if a > b else (a, b)
+    return a, b
+
+def multiplication(a, b):
+    result = 1
+
+    for i in range(a, b + 1):
+        result *= i
+
+    return result
